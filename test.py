5009787e7278d7a25dc767193bfe81195b0be482
gayathri7022 <gayathria7022@gmail.com>
2025-04-30 11:38:08 +0530

    Adding new files

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from Library import register, reserve_book, search_book, login, view_reserve, li
 def test_cases():
     books.clear()
     lib.clear()
-
     res1 = register("gayathri", "gayathri@gmail.com", "12345678")
     assert res1 == "Registeration successfull"
 
@@ -14,10 +13,10 @@ def test_cases():
     assert res3 == "Invalid password"
 
     res4 = search_book("Romeo and juliet", "gayathri@gmail.com")
-    assert res4 == "Book not found"
+    assert res4 == "Book found"
 
     res5 = search_book("XYZ", "gayathri@gmail.com")
-    assert res5 == "Book not found"
+    assert res5 == "Book not available"
 
     res6 = view_reserve('gayathri@gmail.com')
     assert res6 == "No book is reserved"
